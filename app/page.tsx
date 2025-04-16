be3ecd64820b26136209e1c8313a6f8b78e5f1d3
Kickblip <wyatt.zilker@gmail.com>
2025-04-16 17:21:14 -0500

    fix static params error

--- app/page.tsx
+++ app/page.tsx
@@ -4,7 +4,7 @@ import Link from "next/link"
 export default function Home() {
   return (
     <main className="flex flex-col items-center min-h-screen w-full font-mono">
-      <div className="w-full max-w-7xl mx-auto p-4 flex flex-col items-center">
+      <div className="w-full max-w-5xl mx-auto p-4 flex flex-col items-center">
         <Leaderboard />
       </div>
       <Link href="/upload" className="text-blue-500 mt-4 mb-8 hover:underline">
--- app/player/[player_name]/page.tsx
+++ app/player/[player_name]/page.tsx
@@ -1,12 +1,14 @@
 import Link from "next/link"
 
-export default function PlayerPage({ params }: { params: { player_name: string } }) {
+export default async function PlayerPage({ params }: { params: Promise<{ player_name: string }> }) {
+  const { player_name } = await params
+
   return (
     <div className="flex flex-col">
       <Link href="/" className="text-blue-500 hover:underline">
         Go to Home Page
       </Link>
-      <h1 className="text-2xl font-bold mt-4">Player: {params.player_name}</h1>
+      <h1 className="text-2xl font-bold mt-4">Player: {player_name}</h1>
     </div>
   )
 }
