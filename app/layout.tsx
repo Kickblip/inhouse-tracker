f4de2de7dedc93efeb1467f02c2137677878df08
Kickblip <wyatt.zilker@gmail.com>
2025-04-21 19:45:41 -0500

    vercel analytics

--- app/layout.tsx
+++ app/layout.tsx
@@ -3,6 +3,7 @@ import { Geist, Geist_Mono } from "next/font/google"
 import "./globals.css"
 import { ToastContainer } from "react-toastify"
 import UserSearchBar from "@/components/UserSearchBar"
+import { Analytics } from "@vercel/analytics/react"
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
@@ -27,6 +28,7 @@ export default function RootLayout({
   return (
     <html lang="en">
       <body className={`${geistSans.variable} ${geistMono.variable} font-mono antialiased vsc-initialized`}>
+        <Analytics />
         <ToastContainer />
         <div className="flex flex-col">
           <UserSearchBar />
