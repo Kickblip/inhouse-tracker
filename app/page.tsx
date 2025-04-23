8c2ce7c9eaf992b652acc40613948d1d44c78983
Kickblip <wyatt.zilker@gmail.com>
2025-04-22 21:10:58 -0500

    temp remove upload button

--- app/page.tsx
+++ app/page.tsx
@@ -7,9 +7,9 @@ export default function Home() {
       <div className="w-full max-w-5xl mx-auto p-4 flex flex-col items-center">
         <Leaderboard />
       </div>
-      <Link href="/upload" className="text-blue-500 mt-4 mb-8 hover:underline">
+      {/* <Link href="/upload" className="text-blue-500 mt-4 mb-8 hover:underline">
         Go to Upload Page
-      </Link>
+      </Link> */}
     </main>
   )
 }
