e1154b21e57516d4eb43017473cdfcc3420a1e39
Kickblip <wyatt.zilker@gmail.com>
2025-07-19 11:43:07 -0600

    fixed some missing and old splash arts

--- app/(home)/components/RecentlyAddedGame.tsx
+++ app/(home)/components/RecentlyAddedGame.tsx
@@ -1,5 +1,6 @@
 import { Match } from "@/types/Match"
 import Image from "next/image"
+import Link from "next/link"
 
 export function MatchupLine({
   BluePlayer,
@@ -65,34 +66,36 @@ export default function RecentlyAddedGame({ match }: { match: Match }) {
   const team2 = match.participants.slice(match.participants.length / 2)
 
   return (
-    <div
-      className="
+    <Link href={`/m/${match.matchId}`} className="cursor-pointer">
+      <div
+        className="
         relative w-full h-80 rounded-lg bg-slate-900/50
         before:absolute before:inset-y-0 before:left-0 before:w-2
         before:rounded-l before:bg-blue-500 before:content-['']
         after:absolute after:inset-y-0 after:right-0 after:w-2
         after:rounded-r after:bg-red-500 after:content-['']
         "
-    >
-      <div className="py-3 px-6 flex flex-col gap-2 h-full">
-        <div className="flex item-center justify-between text-sm">
-          <h2 className="font-bold">Recent Game</h2>
-          <p className="opacity-80">{timeAgo(match.timestamps.gameEndTimestamp)}</p>
-        </div>
-        <div className="h-full">
-          {team1.map((_, idx) => (
-            <MatchupLine
-              key={idx}
-              BluePlayer={team1[idx].riotIdGameName}
-              RedPlayer={team2[idx].riotIdGameName}
-              BluePlayerKDA={[team1[idx].kills, team1[idx].deaths, team1[idx].assists]}
-              RedPlayerKDA={[team2[idx].kills, team2[idx].deaths, team2[idx].assists]}
-              BlueChampion={team1[idx].championName}
-              RedChampion={team2[idx].championName}
-            />
-          ))}
+      >
+        <div className="py-3 px-6 flex flex-col gap-2 h-full">
+          <div className="flex item-center justify-between text-sm">
+            <h2 className="font-bold">Recent Game</h2>
+            <p className="opacity-80">{timeAgo(match.timestamps.gameEndTimestamp)}</p>
+          </div>
+          <div className="h-full">
+            {team1.map((_, idx) => (
+              <MatchupLine
+                key={idx}
+                BluePlayer={team1[idx].riotIdGameName}
+                RedPlayer={team2[idx].riotIdGameName}
+                BluePlayerKDA={[team1[idx].kills, team1[idx].deaths, team1[idx].assists]}
+                RedPlayerKDA={[team2[idx].kills, team2[idx].deaths, team2[idx].assists]}
+                BlueChampion={team1[idx].championName}
+                RedChampion={team2[idx].championName}
+              />
+            ))}
+          </div>
         </div>
       </div>
-    </div>
+    </Link>
   )
 }
